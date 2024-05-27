862dd66bde7232fa84a244cc6668beb7ae9fc56b
Aben <127937158+Ab3nXY@users.noreply.github.com>
2024-05-27 10:32:25 +0300

    Update settings.py

--- ethiotalentx/settings.py
+++ ethiotalentx/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files (Uploaded by users)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
 
 
 # Default primary key field type
